fd8a7e1db962f915b7c34b0b5fbba0986e9aa4e3
eamiear <2639121731@qq.com>
2018-08-14 21:17:50 +0800

    [ura-mobile-ui] styles

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <header class="ura-header" :class="{'is-fixed': fixed}">
+    <div class="ura-header-button is-left">
+      <slot name="left"></slot>
+    </div>
+    <h1 class="ura-header-title" v-text="title"></h1>
+    <div class="ura-header-button is-right">
+      <slot name="right"></slot>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    fixed: Boolean,
+    title: String
+  }
+}
+</script>
+
+<style lang="css">
+@import '../assets/style/var.css'
+
+@component-namespace ura {
+  @component header {
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    font-size: 14px;
+    color: $color-white;
+    background-color: $color-blue;
+
+  }
+}
+</style>
